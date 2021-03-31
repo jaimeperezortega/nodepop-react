57a058736d031f4e80e4f404b22f2fcf83aa9607
jaimeperezortega <jaimeperezortegadev@gmail.com>
2021-03-31 16:25:36 +0200

    Seteo el estado isinitallyLogged en index.js comprobando si hay o no accessToken guardado en el localStorage

--- nodepop-react/src/App.js
+++ nodepop-react/src/App.js
@@ -4,8 +4,8 @@ import AdsPage from './components/ads/AdsPage'
 import LoginPage from './components/auth/LoginPage'
 import React from 'react';
 
-function App() {
-const [isLogged, setIsLogged] = React.useState(false); //Seteamos el estado de si un usuario está logeado o no en el nivel más alto de nuestra aplicación (App.js) para que pueda funcionar de un modo u otro en función de ese estado. El login se va a producir en el login page porque es donde se va a disparar la acción. Por ello, hay que definir en el Login Page una prop que responda a eso. Vamos a llamar a ese método onLogin. Digamos que LoginPage utiliza este evento para informar a su padre (App.js) de que un usuario está o no logeado
+function App({ isInitiallyLogged }) {
+const [isLogged, setIsLogged] = React.useState(isInitiallyLogged); //Seteamos el estado de si un usuario está logeado o no en el nivel más alto de nuestra aplicación (App.js) para que pueda funcionar de un modo u otro en función de ese estado. El login se va a producir en el login page porque es donde se va a disparar la acción. Por ello, hay que definir en el Login Page una prop que responda a eso. Vamos a llamar a ese método onLogin. Digamos que LoginPage utiliza este evento para informar a su padre (App.js) de que un usuario está o no logeado
 
 const handleLogin = () => setIsLogged(true); //Cuando en el LoginPage se produzca el evento onLogin, se va a disparar el método handleLogin que lo único que hace es cambiar el estado isLogged a true
 
--- nodepop-react/src/index.js
+++ nodepop-react/src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { configureClient } from './api/client';
 require('dotenv').config()
 
-
+const accessToken = localStorage.getItem('token');
+console.log(accessToken);
 
 //Fichero principal de mi App de React. Aquí voy a definir cual va a ser elemento HTML del DOM en el que voy a renderizar mi app React
 
-ReactDOM.render(<App />, document.getElementById('root')
+ReactDOM.render(
+    <App isInitiallyLogged= {!!accessToken/* Con esta expresión conseguimos el booleano de esa expresión, en este caso si noy hay token y es un null es un false, si hay algo es un true */} />, 
+    document.getElementById('root')
 );
 
